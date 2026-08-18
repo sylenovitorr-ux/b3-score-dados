ced5b2906d4a45f83dca8126a4af992f4aa89510
sylenovitorr-ux <sylenovitorr@gmail.com>
2026-08-17 20:34:30 -0400

    switch asset analysis to real tabs

--- site/src/AssetIntelligencePanel.jsx
+++ site/src/AssetIntelligencePanel.jsx
@@ -1,23 +1,18 @@
 import { useMemo } from "react";
-import AssetIntelligencePanelCore from "./AssetIntelligencePanelCore.jsx";
 import AssetSummaryPanel from "./AssetSummaryPanel.jsx";
-import TradeSignalPanel from "./TradeSignalPanel.jsx";
-import DailyHistoryPanel from "./DailyHistoryPanel.jsx";
+import AssetAnalysisTabs from "./AssetAnalysisTabs.jsx";
 import { buildQuantAnalysis } from "./quant/quant-engine.js";
 import "./trade-signal.css";
 import "./theme-premium.css";
 import "./usability-v3.css";
 import "./ux-platform.css";
+import "./asset-tabs.css";
 
 export default function AssetIntelligencePanel(props) {
   const { asset, assets, anomaly } = props;
   const analysis = useMemo(() => buildQuantAnalysis(asset, assets, anomaly), [asset, assets, anomaly]);
-  const book = asset?.book ?? null;
-
   return <>
     <AssetSummaryPanel asset={asset} />
-    <TradeSignalPanel asset={asset} analysis={analysis} book={book} />
-    <DailyHistoryPanel series={anomaly?.series ?? []} ticker={asset.ticker} />
-    <AssetIntelligencePanelCore {...props} />
+    <AssetAnalysisTabs asset={asset} analysis={analysis} anomaly={anomaly} coreProps={props} />
   </>;
 }
