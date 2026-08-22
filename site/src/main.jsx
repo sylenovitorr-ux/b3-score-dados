c3c1fd7df209f791f43a2f6e5eb9b79b6d64d728
sylenovitorr-ux <sylenovitorr@gmail.com>
2026-08-22 10:35:11 -0400

    load prospective validation styles

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./runtime-autoupdate.js";
 import "./styles.css";
 import "./v2.css";
+import "./model-validation.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
