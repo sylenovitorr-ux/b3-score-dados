dd609cb0b3b779ad1d1d1cce9ede01545386faed
sylenovitorr-ux <sylenovitorr@gmail.com>
2026-08-30 17:46:40 -0400

    refactor: ativar interface enxuta sem FIIs e opções

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./AppLite";
 import "./runtime-autoupdate.js";
 import "./styles.css";
 import "./v2.css";
